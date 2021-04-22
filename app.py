86498d59979446245d06ba30a5acd2d9257bca2d
DESKTOP-9OJRH86\User <bsturk.f@gmail.com>
2021-04-22 10:56:46 +0300

    Create Endpoints

--- app.py
+++ app.py
@@ -3,9 +3,32 @@ import dbconnection as db
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["GET","POST"])
 def index():
+    return "Anasayfa"
+
+
+@app.route("/addPerson" , methods = ["POST"])
+def addPerson():
    # db.addUser()
+    return "Anasayfa"
+
+
+@app.route("/deletePerson/<string:name>", methods = ["DELETE"])
+def deletePerson(name):
+   
+    return "Anasayfa"
+
+
+@app.route("/updatePerson/<string:name>", methods = ["PUT"])
+def updatePerson():
+   
+    return "Anasayfa"
+
+
+@app.route("/searchPerson", methods = ["GET"])
+def searchPerson():
+   
     return "Anasayfa"
 
 if __name__ == "__main__":
