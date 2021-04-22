229949110f3e837c6f362dc59c6ac24802738b73
DESKTOP-9OJRH86\User <bsturk.f@gmail.com>
2021-04-22 19:23:49 +0300

    Create endpoint and edited

--- app.py
+++ app.py
@@ -1,35 +1,157 @@
-from flask import Flask
+from flask import Flask ,request , make_response, jsonify
 import dbconnection as db
+import re
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET","POST"])
 def index():
-    return "Anasayfa"
+  #  db.createSchema()
+    return response_success(None,200)
 
 
 @app.route("/addPerson" , methods = ["POST"])
 def addPerson():
-   # db.addUser()
-    return "Anasayfa"
+    data=request.get_json()
 
+    if data is None:
+        return response_error("There is not the json in the body!",400)
+
+    resultCheck= check_addPerson(data)
+    if resultCheck[0]:
+       return response_error(resultCheck[1],400)
+
+    dbResult= db.addPerson(data)
+    if not dbResult[0]:
+        return response_error(dbResult[1],400)
+    return response_success(None,201)
 
 @app.route("/deletePerson/<string:name>", methods = ["DELETE"])
 def deletePerson(name):
-   
-    return "Anasayfa"
 
+    resultCheckName = check_name(name)
+    if resultCheckName[0]:
+       return resultCheckName
+
+    dbResult= db.deletePerson(name)
+    if not dbResult[0]:
+        return response_error(dbResult[1],400) 
+   
+    return response_success(name+" is deleted",200)
 
 @app.route("/updatePerson/<string:name>", methods = ["PUT"])
-def updatePerson():
+def updatePerson(name):
+
+    data=request.get_json()
+
+    if data is None:
+        return response_error("There is not the json in the body!",400)
+
+    resultCheck= check_updatePerson(data)
+    if resultCheck[0]:
+       return response_error(resultCheck[1],400)
+
+    dbResult= db.updatePerson(name,data)
+    if not dbResult[0]:
+        return response_error(dbResult[1],400) 
    
-    return "Anasayfa"
+    return response_success(None,204)
 
 
-@app.route("/searchPerson", methods = ["GET"])
-def searchPerson():
+@app.route("/getPerson/<string:data>", methods = ["GET"])
+def getPerson(data):
+
+    dbResult= db.getPerson(data)
+    if not dbResult[0]:
+        return response_error(dbResult[1],400) 
    
-    return "Anasayfa"
+    return response_success(dbResult[1],200)
+
+def check_addPerson(data):
+    resultCheckData= check_request_data(data)
+    if resultCheckData[0]:
+       return  resultCheckData
+
+    resultCheckName = check_name(data['name'])
+    if resultCheckName[0]:
+       return resultCheckName
+
+    if 'email' in data:
+        resultCheckName = check_email(data['email'])
+        if resultCheckName[0]:
+            return resultCheckName
+    else :
+        data['email']='Null'
+
+    return [False,""]
+
+def check_updatePerson(data):
+    if 'name' in data:
+        resultCheckName = check_name(data['name'])
+        if resultCheckName[0]:
+            return resultCheckName
+
+    if 'email' in data:
+        resultCheckName = check_email(data['email'])
+        if resultCheckName[0]:
+            return resultCheckName
+
+    return [False,""]
+
+def check_request_data(data):
+    message ="'"
+    isError =False
+    if not 'name' in data:
+        message += "name, "
+        isError =True
+
+    if not 'address' in data:
+        message += "address, "
+        isError =True
+
+    if not (('phone' in data) or ('mobilePhone' in data)) :
+        message += "phone(or mobilePhone), "
+        isError =True
+    elif not 'phone' in data:
+        data['phone'] ='Null'
+    elif not 'mobilePhone' in data:
+        data['mobilePhone'] ='Null'
+    
+    if isError:
+        message = message[:-2] + "' request öğeleri boş bırakılamaz!"
+    return [isError,message]
+
+def check_name(name):
+  #  '([A-Z][a-z][a-z]*)  *([A-Z][a-z]*)\\.?  *([A-Z][a-z][a-z][a-z]*)'
+  # '[A-Za-z]{2,25}( [A-Za-z]{2,25})?( [A-Za-z]{2,25})?'
+    message =""
+    isError =False
+    if not re.fullmatch('[A-Za-z]{2,25}( [A-Za-z]{2,25})?( [A-Za-z]{2,25})?', name):
+        message += "'name' is invalid!"
+        isError =True
+    
+    return [isError,message]
+
+def check_email(email):
+  # '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    message =""
+    isError =False
+    if not re.fullmatch('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', email):
+        message += "'email' is invalid!"
+        isError =True
+    
+    return [isError,message]
+
+def response_error(message, status_code): 
+    if " gives an error" in message:
+        status_code=500
+    message = {'errorCode': status_code, 'errorMessage': message}
+    return make_response(jsonify(message), status_code)
+
+def response_success(message, status_code): 
+    if None is not  message:
+        message = {'succesCode': status_code, 'data': message}
+    return make_response(jsonify(message), status_code)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- dbconnection.py
+++ dbconnection.py
@@ -9,22 +9,134 @@ def createSchema():
     checkConnection()
     cur = con.cursor()
     try:
-        cur.execute("create database TestDB")
-        cur.execute("create table ")
+       # cur.execute("create database TestDB")
+        cur.execute("CREATE TABLE testdb.addressbook (id INT NOT NULL AUTO_INCREMENT,name VARCHAR(45) NOT NULL, address VARCHAR(300) NOT NULL, phone BIGINT(10) NULL, mobilePhone BIGINT(10) NULL, email VARCHAR(45) NULL, PRIMARY KEY (id, name));")
     except:
         con.rollback()
     cur.close()
 
-def addUser():
+def addPerson(personData):
+    personNameResult= isPersonName(personData['name'])
+    
+    if personNameResult[0]:
+        personNameResult[0]=False
+        return personNameResult
+    elif " gives an error" in personNameResult[1]:
+        return personNameResult
+
     checkConnection()
     cur = con.cursor()
     try:
-        cur.execute("insert into deneme (aa, bb, cc) values ('Bill','Gates','test')")  
+        cur.execute("insert into testdb.addressbook (name, address, phone, mobilePhone, email) values ('"+personData['name']+"','"+personData['address']+"',"+str(personData['phone'])+", "+str(personData['mobilePhone'])+",'"+personData['email']+"');")  
         con.commit()
+        return [True,""]
     except:
-        con.rollback()
-    cur.close()
-    con.close()
+        return [False,"It gives an error while adding it to the database."]
+    finally:
+        cur.close()
+        con.close()
+
+def deletePerson(personName):
+    personNameResult= isPersonName(personName)
+    
+    if not personNameResult[0]:
+        return personNameResult
+    elif " gives an error" in personNameResult[1]:
+        return personNameResult
+
+    checkConnection()
+    cur = con.cursor()
+    try:
+        cur.execute("delete from testdb.addressbook WHERE name='"+personName+"';")  
+        con.commit()
+        return [True,""]
+    except:
+        return [False,"It gives an error while adding it to the database."]
+    finally:
+        cur.close()
+        con.close()
+
+def updatePerson(personName,data):
+    dbData=getPerson(personName)
+    if not dbData[0]:
+        return dbData
+    elif " gives an error" in dbData[1]:
+        return dbData
+
+    tempData = dbData[1].copy()
+    k1 = set(tempData.keys())
+    k2 = set(data.keys())
+    common_keys = set(k1).intersection(set(k2))
+
+    for key in common_keys:
+        if tempData[key] != data[key] :
+            tempData[key] = data[key]
+
+    checkConnection()
+    cur = con.cursor()
+    try:
+        cur.execute("UPDATE testdb.addressbook SET name='"+tempData['name']+"', address='"+tempData['address']+"', phone="+str(tempData['phone'])+", mobilePhone="+str(tempData['mobilePhone'])+", email='"+tempData['email']+"' WHERE id = "+str(tempData['id'])+";")  
+        con.commit()
+        return [True,tempData]
+    except:
+        return [False,"It gives an error while adding it to the database."]
+    finally:
+        cur.close()
+        con.close()
+
+def getPerson(data):
+    searchData= {}
+    if isinstance(data, int):
+        searchData['name']="#####"
+        searchData['address']="#####"
+        searchData['phone']=data
+        searchData['mobilePhone']=data
+        searchData['email']="#####"
+    else:
+        searchData['name']=data
+        searchData['address']=data
+        searchData['phone']="1111"
+        searchData['mobilePhone']="1111"
+        searchData['email']=data
+
+    checkConnection()
+    cur = con.cursor()
+    try:
+        cur.execute("SELECT * FROM testdb.addressbook where name='"+searchData['name']+"' or address='"+searchData['address']+"' or phone="+str(searchData['phone'])+"  or mobilePhone="+str(searchData['mobilePhone'])+"  or email='"+searchData['email']+"';")
+        row = cur.fetchone()
+        if row is None:
+            return [False,"There is not this person in the database."]
+        else:
+            currentData ={}
+            currentData['id']="Null" if row[0] is None else row[0]
+            currentData['name']="Null" if row[1] is None else row[1]
+            currentData['address']="Null" if row[2] is None else row[2]
+            currentData['phone']="Null" if row[3] is None else row[3]
+            currentData['mobilePhone']="Null" if row[4] is None else row[4]
+            currentData['email']="Null" if row[5] is None else row[5]
+            return [True,currentData]
+    except:
+        return [False,"It gives an error while adding it to the database."]
+    finally:
+        cur.close()
+        con.close()
+    
+
+def isPersonName(personName):
+    checkConnection()
+    cur = con.cursor()
+    try:
+        cur.execute("SELECT * FROM testdb.addressbook where name='"+personName+"';")
+        row = cur.fetchone()
+        if row is None:
+            return [False,"There is not this person in the database."]
+        else:
+            return [True,"There is this person in the database."]
+    except:
+        return [False,"It gives an error while adding it to the database."]
+    finally:
+        cur.close()
+        con.close()
 
 def checkConnection():
     if not con.is_connected():
